749f744ece6b753c23a0e5fbd48d8386a6b0cddf
Lucian Trepteanu <lucian.trepteanu@gmail.com>
2022-06-14 18:03:45 +0300

    User can now share basic event data in other apps

--- app/src/main/java/com/example/greenscene/Functionalities/FavouriteConcertsDetails/FavouriteConcertsDetailsFragment.java
+++ app/src/main/java/com/example/greenscene/Functionalities/FavouriteConcertsDetails/FavouriteConcertsDetailsFragment.java
@@ -116,8 +116,9 @@ public class FavouriteConcertsDetailsFragment extends Fragment {
                     @Override
                     public void onClick(View view) {
                         Intent sendIntent = new Intent(Intent.ACTION_SEND);
-                        String eventData = currentEvent.getTitle() + " @ " + currentEvent.getStart();
+                        String eventData = currentEvent.getTitle() + " @ " + Utils.prettyFormatDate(currentEvent.getStart());
                         sendIntent.putExtra(Intent.EXTRA_TEXT,eventData);
+                        sendIntent.setType("text/plain");
                         startActivity(Intent.createChooser(sendIntent, "Share Event!"));
                     }
                 });
